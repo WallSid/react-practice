a8f98e6ed26abcea3412d5db7b0947226d11f1c5
Vladyslav Savchuk <103652593+WallSid@users.noreply.github.com>
2024-05-15 20:51:06 +0300

    practice

--- src/App.js
+++ src/App.js
@@ -2,11 +2,34 @@ import React from "react";
 import { useState } from "react";
 import Counter from "./components/Counter";
 import ClassCounter from "./components/ClassCounter";
+import PostItem from "./components/PostItem";
+import './styles/App.css';
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
+  const [posts, setPost] = useState([
+    {id: 1, title: 'JavaScript', body: 'Description'},
+    {id: 2, title: 'JavaScript 2', body: 'Description 2'},
+    {id: 3, title: 'JavaScript 3', body: 'Description 3'},
+    {id: 4, title: 'JavaScript 4', body: 'Description 4'},
+  ])
+  const [posts2, setPost2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python2', body: 'Description 2'},
+    {id: 3, title: 'Python3', body: 'Description 3'},
+    {id: 4, title: 'Python4', body: 'Description 4'},
+  ])
+
   return (
     <div className="App">
-        <ClassCounter/>
+      <form action="submit">
+        <input type="text" placeholder="Post name"/>
+        <input type="text" placeholder="Post description"/>
+        <MyButton>Create post</MyButton>
+      </form>
+        <PostList posts={posts} title='Posts about JS'/>
+        <PostList posts={posts2} title='Posts about Python'/>
     </div>
   );
 }
